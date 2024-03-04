c45dc8bbb146f03667a581008f030bc1bff11ef8
Leommm-byte <leom20031@gmail.com>
2024-03-04 09:43:22 +0100

    Fixed the f-syntax error

--- app/model.py
+++ app/model.py
@@ -16,7 +16,7 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = f'{os.environ.get("SECRET_KEY")}'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://hospitalmanagement.postgres.database.azure.com:5432/postgres?user=hms&password={os.environ.get('PGPASSWORD')}&sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://hospitalmanagement.postgres.database.azure.com:5432/postgres?user=hms&password={os.environ.get("PGPASSWORD")}&sslmode=require'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
